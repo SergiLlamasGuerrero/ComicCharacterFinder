f006fffbdc3fbce15c6a1b5c0a258bab9dd6a142
Sergi Llamas <sllamas@fanscup.com>
2016-08-29 17:41:43 +0200

    First screen container. Main activity

---
+++ app/src/main/java/finder/character/comic/comiccharacterfinder/main/MainActivity.java
@@ -0,0 +1,20 @@
+package finder.character.comic.comiccharacterfinder.main;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import finder.character.comic.comiccharacterfinder.R;
+
+/**
+ * Created by sllamas on 29/8/16.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
